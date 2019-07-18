110b37754283fcc57c59279e106fdb91bb619250
Vadkhar <vad20002016@gmail.com>
2019-07-18 13:37:27 +0200

    first commit;

---
+++ script.js
@@ -0,0 +1,20 @@
+let money = "Ваш бюджет на месяц?",
+time = "Введите дату в формате YYYY-MM-DD";
+
+let appData = {
+budget: money,
+timeData: time,
+expenses: {},
+optionalExpenses: {},
+income: [],
+savings: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом меяце", ''),
+    a2 = prompt("Во сколько обойдется?", ''),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a4 = prompt("Во сколько обойдется?", '');
+
+    appData.expenses.a1=a2;
+    appData.expenses.a3=a4;
+    alert(appData.budget/30);
